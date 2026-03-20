279a10dbbd2a055b2dbd309c4f9c53586b032d0e
camellia71 <2077767215@qq.com>
2026-03-20 21:06:41 +0800

    react-qianfeng

--- react-jike/src/utils/index.js
+++
@@ -1,7 +0,0 @@
-//统一中转工具模块函数
-//例子：
-//import {request} from 'utils';
-import { request } from "./request";
-import { getToken, setToken, removeToken } from "./token";
-
-export { request, getToken, setToken, removeToken };
--- react-router-pro/src/page/Board/index.js
+++
@@ -1,4 +0,0 @@
-const Board = () => {
-  return <div>我是Board页</div>;
-};
-export default Board;
